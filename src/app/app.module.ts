f92ca8a87da5160cd0fc913a73aae57683ae6aa5
Luis Martínez <lemartinezmendoza@gmail.com>
2023-09-08 10:32:06 -0500

    feat: add footer component mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent],
@@ -13,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     HeaderComponent,
+    FooterComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
