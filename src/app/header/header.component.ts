d62cb786b066bed168c8360d8e1c86e6104a24e2
Luis Martínez <lemartinezmendoza@gmail.com>
2023-09-08 09:44:36 -0500

    chore: add header component mockup with routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,28 @@ import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    CommonModule,
+    MatSidenavModule,
+    RouterLink,
+  ],
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isDrawerOpen: boolean = false;
+
+  toogleDrawer() {
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
+}
